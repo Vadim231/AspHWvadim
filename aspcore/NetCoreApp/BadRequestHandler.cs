7047f5c84cf1759b9945b6f1fde86885890750f0
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2019-10-14 20:16:02 +0600

    Add files via upload

---
+++ aspcore/NetCoreApp/BadRequestHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace NetCoreApp
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class BadRequestHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public BadRequestHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = 400;
+                httpContext.Response.ContentType = "text/html";
+                string ms = string.Format($"<h2> {ex.Message} </h2>\r\n <h3>Error code {httpContext.Response.StatusCode }</h3>");//почему не работает html код
+                await httpContext.Response.WriteAsync(ms);
+                return;
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class BadRequestHandlerExtensions
+    {
+        public static IApplicationBuilder UseBadRequestHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BadRequestHandler>();
+        }
+    }
+}
---
+++ aspcore/NetCoreApp/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetCoreApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Enter once in start
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseBadRequestHandler();
+
+            app.Map("/home", home =>
+            {
+                home.Map("/index", Index);
+                home.Map("/about", About);
+
+                home.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Home page");
+                });
+            });
+
+            app.Map("", home =>
+            {
+                home.Run(async context =>
+                {
+                throw new Exception("Incorrect request");
+                    //await context.Response.WriteAsync("Start page");
+                });
+            });
+
+            app.Run(async (context) =>
+            {
+                if (context.Response.StatusCode == 404)
+                    await context.Response.WriteAsync("<h1 style=\"color:red\">Page not found</h1>");
+            });
+        }
+
+        private static void Index(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Index page");
+            });
+        }
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("About page");
+            });
+        }
+    }
+}
